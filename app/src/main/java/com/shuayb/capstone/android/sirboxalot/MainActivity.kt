954da4e113c742067a2ceda2acc5b9fbf2c49fbc
Shuayb Khan <shuaybk@gmail.com>
2020-04-09 15:24:27 -0400

    Add Settings (not populated).  Add pause/resume functionality for timer

--- app/src/main/java/com/shuayb/capstone/android/sirboxalot/MainActivity.kt
+++ app/src/main/java/com/shuayb/capstone/android/sirboxalot/MainActivity.kt
@@ -1,23 +1,34 @@
 package com.shuayb.capstone.android.sirboxalot
 
+import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.shuayb.capstone.android.sirboxalot.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
+    private val TIMER_STATE_STOPPED = "stopped"
+    private val TIMER_STATE_PAUSED = "paused"
+    private val TIMER_STATE_RUNNING = "running"
     private val ROUND_TIME : Int = 15   //In seconds
     private val REST_TIME : Int = 10     //In seconds
     private val NUM_ROUNDS : Int = 3
 
     private lateinit var mBinding : ActivityMainBinding
     private lateinit var mediaPlayer : MediaPlayer
-    private var timeRemaining : Int = 0
+    private lateinit var counterJob : Job
+    private lateinit var counterThread : Thread
+    private var timeRemaining : Int = ROUND_TIME
     private var roundsRemaining : Int = NUM_ROUNDS
-    private var timerRunning : Boolean = false
+    private var timerState : String = TIMER_STATE_STOPPED     //State can be stopped/running/paused
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,9 +39,10 @@ class MainActivity : AppCompatActivity() {
     private fun initSetup() {
         mediaPlayer = MediaPlayer.create(this, R.raw.triple_bell)
         mBinding.startButton.setOnClickListener {
-            if (!timerRunning) {
-                startCounterThread()
-            }
+            startCounterThread()
+        }
+        mBinding.pauseButton.setOnClickListener {
+            pauseCounterThread()
         }
     }
 
@@ -38,12 +50,59 @@ class MainActivity : AppCompatActivity() {
         runOnUiThread {
             mBinding.roundTimeRemaining.setText("$timeRemaining")
             mBinding.roundsRemaining.setText("$roundsRemaining")
+
+            if (timerState == TIMER_STATE_STOPPED) {
+                mBinding.startButton.visibility = View.VISIBLE
+                mBinding.pauseButton.visibility = View.INVISIBLE
+            } else if (timerState == TIMER_STATE_PAUSED) {
+                mBinding.startButton.visibility = View.VISIBLE
+                mBinding.pauseButton.visibility = View.INVISIBLE
+            } else if (timerState == TIMER_STATE_RUNNING) {
+                mBinding.startButton.visibility = View.INVISIBLE
+                mBinding.pauseButton.visibility = View.VISIBLE
+            }
         }
     }
 
     private fun startCounterThread() {
-        timerRunning = true
-        GlobalScope.launch {
+        if (roundsRemaining > 0 && timerState == TIMER_STATE_STOPPED) {
+            mediaPlayer.start()
+            timerState = TIMER_STATE_RUNNING
+        }
+        if (timerState == TIMER_STATE_PAUSED) {
+            //play resume sound
+            timerState = TIMER_STATE_RUNNING
+        }
+        updateTimerViews()
+        counterJob = GlobalScope.launch {
+            while (roundsRemaining > 0) {
+                while (timeRemaining > 0) {
+                    updateTimerViews()
+                    delay(1000)
+                    timeRemaining--
+                }
+                if (roundsRemaining > 1 && REST_TIME > 0) {
+                    timeRemaining += REST_TIME
+                    mediaPlayer.start()
+
+                    while (timeRemaining > 0) {
+                        updateTimerViews()
+                        delay(1000)
+                        timeRemaining--
+                    }
+                }
+                roundsRemaining--
+                if (roundsRemaining > 0) {
+                    timeRemaining += ROUND_TIME
+                    mediaPlayer.start()
+                }
+            }
+            mediaPlayer.start() //End bell
+            timerState = TIMER_STATE_STOPPED
+            updateTimerViews()
+        }
+        /*
+        counterJob = GlobalScope.launch {
             while (roundsRemaining > 0) {
                 timeRemaining += ROUND_TIME
                 mediaPlayer.start()
@@ -68,5 +127,30 @@ class MainActivity : AppCompatActivity() {
             mediaPlayer.start() //End bell
             timerRunning = false
         }
+         */
     }
+
+    private fun pauseCounterThread() {
+        counterJob.cancel()
+        timerState = TIMER_STATE_PAUSED
+        updateTimerViews()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                var intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
