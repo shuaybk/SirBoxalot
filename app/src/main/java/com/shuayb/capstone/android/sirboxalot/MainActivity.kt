b6878d4d28c80abdb7afee80e1d68dd841f7a8f5
Shuayb Khan <shuaybk@gmail.com>
2020-04-10 15:30:26 -0400

    Add Functionality: Retrieve round time/count from settings

--- app/src/main/java/com/shuayb/capstone/android/sirboxalot/MainActivity.kt
+++ app/src/main/java/com/shuayb/capstone/android/sirboxalot/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.shuayb.capstone.android.sirboxalot
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
@@ -11,17 +13,18 @@ import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.shuayb.capstone.android.sirboxalot.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
     private val TIMER_STATE_STOPPED = "stopped"
     private val TIMER_STATE_PAUSED = "paused"
     private val TIMER_STATE_RUNNING = "running"
-    private val ROUND_TIME : Int = 15   //In seconds
-    private val REST_TIME : Int = 10     //In seconds
-    private val PREPARE_TIME : Int = 5     //In seconds
-    private val NUM_ROUNDS : Int = 3
+
+    //Initialize the real values from settings
+    private var NUM_ROUNDS : Int = 0
+    private var ROUND_TIME : Int = 0   //In seconds
+    private var REST_TIME : Int = 0     //In seconds
+    private var PREPARE_TIME : Int = 0     //In seconds
 
     private lateinit var STATUS_PREP : String
     private lateinit var STATUS_FIGHT : String
@@ -32,14 +35,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mBinding : ActivityMainBinding
     private lateinit var mediaPlayer : MediaPlayer
     private lateinit var counterJob : Job
-    private var timeRemaining : Int = ROUND_TIME
-    private var roundsRemaining : Int = NUM_ROUNDS
+    private var timeRemaining : Int = 0
+    private var roundsRemaining : Int = 0
     private var timerState : String = TIMER_STATE_STOPPED     //State can be stopped/running/paused
     private var prepareRequired : Boolean = true
+    private lateinit var sharedPreferences : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         initSetup()
     }
 
@@ -50,6 +55,8 @@ class MainActivity : AppCompatActivity() {
         STATUS_COMPLETE = getString(R.string.status_complete)
         STATUS_PAUSED = getString(R.string.status_paused)
 
+        setValuesFromSettings()
+
         updateTimerViews("")
         mediaPlayer = MediaPlayer.create(this, R.raw.triple_bell)
         mBinding.startButton.setOnClickListener {
@@ -59,18 +66,18 @@ class MainActivity : AppCompatActivity() {
             pauseCounterThread()
         }
         mBinding.resetButton.setOnClickListener {
-            resetCounter()
+            resetButtonPressed()
         }
         mBinding.forwardButton.setOnClickListener {
             timeRemaining = 0;
-        }
-        if (timeRemaining < 0) {
-            timeRemaining = 0
         }
     }
 
     private fun updateTimerViews(currStatus : String) {
         runOnUiThread {
+            if (timeRemaining < 0) {
+                timeRemaining = 0
+            }
             mBinding.timerStatusText.setText(currStatus)
             mBinding.roundTimeRemaining.setText("$timeRemaining")
             mBinding.roundsRemaining.setText("$roundsRemaining")
@@ -155,9 +162,21 @@ class MainActivity : AppCompatActivity() {
             counterJob.cancel()
         }
         timerState = TIMER_STATE_STOPPED
-        roundsRemaining = NUM_ROUNDS
-        timeRemaining = ROUND_TIME
         updateTimerViews("")
+    }
+
+    private fun setValuesFromSettings() {
+        NUM_ROUNDS = sharedPreferences.getString("num_rounds_key", "3").toInt()
+        ROUND_TIME = sharedPreferences.getString("round_time_key", "15").toInt()
+        REST_TIME = sharedPreferences.getString("rest_time_key", "10").toInt()
+        PREPARE_TIME = sharedPreferences.getString("prepare_time_key", "5").toInt()
+        timeRemaining = ROUND_TIME
+        roundsRemaining = NUM_ROUNDS
+    }
+
+    private fun resetButtonPressed() {
+        setValuesFromSettings()
+        resetCounter()
     }
 
     private fun playSound() {
