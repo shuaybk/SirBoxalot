2770e48f4fd07851fbf769bf4398ecb503666a5a
Shuayb Khan <shuaybk@gmail.com>
2020-04-11 17:04:22 -0400

    Make the sound settings play a preview of the sound when the setting is changed.

--- app/src/main/java/com/shuayb/capstone/android/sirboxalot/SettingsActivity.kt
+++ app/src/main/java/com/shuayb/capstone/android/sirboxalot/SettingsActivity.kt
@@ -1,11 +1,14 @@
 package com.shuayb.capstone.android.sirboxalot
 
 import android.content.SharedPreferences
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -45,10 +48,38 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         override fun onSharedPreferenceChanged(
-            sharedPreferences: SharedPreferences?,
+            sharedPreferences: SharedPreferences,
             key: String?
         ) {
-            Toast.makeText(context, getString(R.string.toast_settings_change), Toast.LENGTH_LONG).show()
+            when (key) {
+                "round_start_sound" -> launchSoundThread(sharedPreferences.getString(key, ""))
+                "round_end_sound" -> launchSoundThread(sharedPreferences.getString(key, ""))
+                "round_end_warn_sound" -> launchSoundThread(sharedPreferences.getString(key, ""))
+                "rest_end_warn_sound" -> launchSoundThread(sharedPreferences.getString(key, ""))
+                "inter_round_alert_sound" -> launchSoundThread(sharedPreferences.getString(key, ""))
+                "pause_resume_sound" -> launchSoundThread(sharedPreferences.getString(key, ""))
+                else -> Toast.makeText(context, getString(R.string.toast_settings_change), Toast.LENGTH_LONG).show()
+            }
+        }
+
+        private fun launchSoundThread(rawResValue : String?) {
+            var rawResId = 0
+            val soundValues = getResources().obtainTypedArray(R.array.sound_values)
+            val strArray = resources.getStringArray(R.array.sound_values)
+
+            //Determine the resource ID for the string raw resource
+            for (i in 0 .. strArray.size - 1) {
+                if (strArray[i] == rawResValue) {
+                    rawResId = soundValues.getResourceId(i, 0)
+                    break
+                }
+            }
+            
+            GlobalScope.launch {
+                val mpSound = MediaPlayer.create(context, rawResId)
+                mpSound.start()
+            }
+            soundValues.recycle()
         }
     }
 }
