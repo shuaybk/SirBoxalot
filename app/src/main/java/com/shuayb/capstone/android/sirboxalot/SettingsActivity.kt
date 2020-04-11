5b0584805ecfbd252ae9d08adaac0a397bee249a
Shuayb Khan <shuaybk@gmail.com>
2020-04-11 16:42:10 -0400

    Display Toast message on settings change

--- app/src/main/java/com/shuayb/capstone/android/sirboxalot/SettingsActivity.kt
+++ app/src/main/java/com/shuayb/capstone/android/sirboxalot/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.shuayb.capstone.android.sirboxalot
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 
@@ -27,9 +29,26 @@ class SettingsActivity : AppCompatActivity() {
         }
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+
+        override fun onResume() {
+            super.onResume()
+            preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+        }
+
+        override fun onPause() {
+            super.onPause()
+            preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        }
+
+        override fun onSharedPreferenceChanged(
+            sharedPreferences: SharedPreferences?,
+            key: String?
+        ) {
+            Toast.makeText(context, getString(R.string.toast_settings_change), Toast.LENGTH_LONG).show()
         }
     }
 }
